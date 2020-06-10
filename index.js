0feb13e123e08a4050934233abc34e70c65d63f9
Jason Bokesch <jbokesch7628@gmail.com>
2020-06-10 17:54:56 -0400

    made final changes to index.js file

--- index.js
+++ index.js
@@ -39,9 +39,26 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+
+Person.prototype.eat = function(food) {
+  if (this.stomach.length < 10) {
+    return this.stomach.push(food);
+  }
+};
+
+Person.prototype.poop = function() {
+  this.stomach = [];
+  return this.stomach;
+};
+
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`;
+};
 
 /*
   TASK 2
@@ -57,9 +74,16 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.tank = 0;
+  this.odometer = 0;
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
 }
+
+Car.prototype.fill = function(gallons) {
+  return (this.tank += gallons);
+};
 
 /*
   TASK 3
@@ -68,18 +92,31 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, favoriteToy);
+  this.favoriteToy = favoriteToy;
+  this.name = name;
+  this.age = age;
 }
+
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`;
+};
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. If the function is within the global scope, then the value of "this" inside of
+  that function will in fact be the window object.
+  2. If a function is called by a preceeding dot, then "this" is the object before
+  the dot.
+  3. When a constructor function is used, a specific instance of the object is 
+  created and then returned by the constructor. 
+  4. When a call or array method is used, "this" is explicity defined.
+*/
 */
 
 
